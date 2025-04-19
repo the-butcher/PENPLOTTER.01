f81d284ad619a055e56fd0b10cfe70df9e21a645
VERTIGIS\hannes.fleischer <hannes.fleischer@vertigis.com>
2025-04-19 18:35:46 +0200

    cleanup

--- pp_contour/src/root.tsx
+++
@@ -1,4 +0,0 @@
-import { createRoot } from "react-dom/client";
-import RootApp from "./RootApp";
-
-createRoot(document.getElementById('rootdiv')!).render(<RootApp />);
--- pp_waves/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from "react-dom";
-import App from "./App";
-import './fonts/smb.ttf';
-
-render(<App />, document.getElementById("rootdiv"));
-
