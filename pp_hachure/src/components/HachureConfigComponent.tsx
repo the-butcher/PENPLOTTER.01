2e0dd241f350559850b1a62581c43bd13a9985c3
VERTIGIS\hannes.fleischer <hannes.fleischer@vertigis.com>
2025-04-19 11:43:45 +0200

    ui refinements

--- pp_hachure/src/components/HachureConfigComponent.tsx
+++ pp_hachure/src/components/HachureConfigComponent.tsx
@@ -1,6 +1,6 @@
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import { Button, Divider, Grid, TextField } from "@mui/material";
+import { Button, Divider, FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import { IActiveStepProps } from './IActiveStepProps';
 import { IHachureConfigProps } from './IHachureConfigProps';
@@ -61,6 +61,26 @@ function HachureConfigComponent(props: IHachureConfigProps & IActiveStepProps) {
 
     const handleMaxSpacingInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setMaxSpacingInt(event.target.value === '' ? maxSpacingInt : Math.max(getMaxSpacingLimit(), Number(event.target.value)))
+    };
+
+    const handleContourOffInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setContourOffInt(event.target.value === '' ? contourOffInt : Number(event.target.value))
+    };
+
+    const handleContourDivInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setContourDivInt(event.target.value === '' ? contourDivInt : Number(event.target.value))
+    };
+
+    const handleHachureDegInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setHachureDegInt(event.target.value === '' ? hachureDegInt : Number(event.target.value))
+    };
+
+    // const handleContourDspInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setContourDspInt(event.target.value === '' ? contourDspInt : Number(event.target.value))
+    // };
+
+    const handleContourDspSelectChange = (event: SelectChangeEvent<number>) => {
+        setContourDspInt(event.target.value === '' ? contourDspInt : Number(event.target.value))
     };
 
     const getMinSpacingLimit = () => {
@@ -130,6 +150,7 @@ function HachureConfigComponent(props: IHachureConfigProps & IActiveStepProps) {
                             shrink: true
                         }
                     }}
+                    helperText={showHelperTexts ? 'the minimum spacing between hachure lines. if hachures become too close, one of the lines is discontinued.' : undefined}
                 />
             </Grid>
             <Grid item xs={12}>
@@ -154,7 +175,133 @@ function HachureConfigComponent(props: IHachureConfigProps & IActiveStepProps) {
                             shrink: true
                         }
                     }}
+                    helperText={showHelperTexts ? 'the maximum spacing between hachure lines. if hachures get too far apart, a new hachure line is started between.' : undefined}
                 />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    label={'contour vertical interval (m)'}
+                    value={contourOffInt}
+                    type="number"
+                    variant="outlined"
+                    onChange={handleContourOffInputChange}
+                    disabled={activeStep !== STEP_INDEX_HACHURE__CONFIG}
+                    sx={{
+                        width: '100%'
+                    }}
+                    slotProps={{
+                        htmlInput: {
+                            step: 0.1,
+                            min: 0.1,
+                            max: 10,
+                            type: 'number'
+                        },
+                        inputLabel: {
+                            shrink: true
+                        }
+                    }}
+                    helperText={showHelperTexts ? 'the vertical distance in meters between contours during processing. low values (i.e. 0.5) produce more detail, high values (i.e. 5.0) produce less detail, but are faster.' : undefined}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    label={'contour segment length (m)'}
+                    value={contourDivInt}
+                    type="number"
+                    variant="outlined"
+                    onChange={handleContourDivInputChange}
+                    disabled={activeStep !== STEP_INDEX_HACHURE__CONFIG}
+                    sx={{
+                        width: '100%'
+                    }}
+                    slotProps={{
+                        htmlInput: {
+                            step: 0.5,
+                            min: 1,
+                            max: 10,
+                            type: 'number'
+                        },
+                        inputLabel: {
+                            shrink: true
+                        }
+                    }}
+                    helperText={showHelperTexts ? 'the segment length in meters used to divide contours during processing. low values (i.e. 2.0) produce more detail, high values (i.e. 5.0) produce less detail, but are faster.' : undefined}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    label={'minimum slope (deg)'}
+                    value={hachureDegInt}
+                    type="number"
+                    variant="outlined"
+                    onChange={handleHachureDegInputChange}
+                    disabled={activeStep !== STEP_INDEX_HACHURE__CONFIG}
+                    sx={{
+                        width: '100%'
+                    }}
+                    slotProps={{
+                        htmlInput: {
+                            step: 0.1,
+                            min: 1,
+                            max: 20,
+                            type: 'number'
+                        },
+                        inputLabel: {
+                            shrink: true
+                        }
+                    }}
+                    helperText={showHelperTexts ? 'the minimum slope in degrees for hachure lines to be drawn. low values (i.e. 2.0) can produce more detail, but may give undesired artifacts, high values (i.e. 10.0) will miss details in terrain that is less pronounced.' : undefined}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">contour display interval (m)</InputLabel>
+                    <Select
+                        value={contourDspInt}
+                        label="contour display interval (m)"
+                        onChange={handleContourDspSelectChange}
+                        disabled={activeStep !== STEP_INDEX_HACHURE__CONFIG}
+                    >
+                        <MenuItem value={5}>5</MenuItem>
+                        <MenuItem value={10}>10</MenuItem>
+                        <MenuItem value={20}>20</MenuItem>
+                        <MenuItem value={25}>25</MenuItem>
+                        <MenuItem value={25}>25</MenuItem>
+                        <MenuItem value={50}>50</MenuItem>
+                        <MenuItem value={100}>100</MenuItem>
+                        <MenuItem value={200}>200</MenuItem>
+                        <MenuItem value={250}>250</MenuItem>
+                        <MenuItem value={500}>500</MenuItem>
+                    </Select>
+                    {
+                        showHelperTexts ? <FormHelperText>the vertical distance in meters between contours to be added to the output.</FormHelperText> : null
+                    }
+                </FormControl>
+
+
+                {/* <TextField
+                    label={'contour display interval (m)'}
+                    value={hachureDegInt}
+                    type="number"
+                    variant="outlined"
+                    onChange={handleHachureDegInputChange}
+                    disabled={activeStep !== STEP_INDEX_HACHURE__CONFIG}
+                    sx={{
+                        width: '100%'
+                    }}
+                    slotProps={{
+                        htmlInput: {
+                            step: 1,
+                            min: 5,
+                            max: 500,
+                            type: 'number'
+                        },
+                        inputLabel: {
+                            shrink: true
+                        }
+                    }}
+                    helperText={showHelperTexts ? 'the minimum slope in degrees for hachure lines to be drawn. low values (i.e. 2.0) can produce more detail, but may give undesired artifacts, high values (i.e. 10.0) will miss details in terrain that is less pronounced.' : undefined}
+                /> */}
             </Grid>
             {
                 activeStep === STEP_INDEX_HACHURE__CONFIG ? <>
